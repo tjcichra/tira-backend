70999f3df504ebadf5a75129b58a6d8d7bb96c1d
Justin Cichra <1342149+jrcichra@users.noreply.github.com>
2022-02-19 16:36:39 -0500

    fix amend syntax

--- .github/build/manifest.sh
+++ .github/build/manifest.sh
@@ -2,13 +2,13 @@
 set -x
 # do it for the sha
 export TIRA_SHA="sha-${TIRA_SHA::8}"
-docker manifest create ghcr.io/tjcichra/tira_backend:${TIRA_SHA} --amend \
-    ghcr.io/tjcichra/tira_backend_amd64:${TIRA_SHA} \
-    ghcr.io/tjcichra/tira_backend_arm64:${TIRA_SHA}
+docker manifest create ghcr.io/tjcichra/tira_backend:${TIRA_SHA} \
+    --amend ghcr.io/tjcichra/tira_backend_amd64:${TIRA_SHA} \
+    --amend ghcr.io/tjcichra/tira_backend_arm64:${TIRA_SHA}
 docker manifest push ghcr.io/tjcichra/tira_backend:${TIRA_SHA}
 # now for latest
 export TIRA_SHA="latest"
-docker manifest create ghcr.io/tjcichra/tira_backend:${TIRA_SHA} --amend \
-    ghcr.io/tjcichra/tira_backend_amd64:${TIRA_SHA} \
-    ghcr.io/tjcichra/tira_backend_arm64:${TIRA_SHA}
+docker manifest create ghcr.io/tjcichra/tira_backend:${TIRA_SHA} \
+    --amend ghcr.io/tjcichra/tira_backend_amd64:${TIRA_SHA} \
+    --amend ghcr.io/tjcichra/tira_backend_arm64:${TIRA_SHA}
 docker manifest push ghcr.io/tjcichra/tira_backend:${TIRA_SHA}
