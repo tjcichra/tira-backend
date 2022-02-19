4a31f4b2c3ad32eabc6509d211456484112c9de7
Justin Cichra <1342149+jrcichra@users.noreply.github.com>
2022-02-19 02:29:20 -0500

    export jobname

--- .github/build/script.sh
+++ .github/build/script.sh
@@ -2,7 +2,7 @@
 set -x
 export TIRA_SHA=${TIRA_SHA::8}
 JSON=$(cat .github/build/request.json | envsubst)
-JOBNAME=$(curl --fail -X POST -s -H 'Content-Type: application/json' -H "CF-Access-Client-Id: 46ea1be6ad585778416864f114d5cff1.access" -H "CF-Access-Client-Secret: ${ORACLE_K8S_ACCESS_TOKEN}" -d "${JSON}" https://kaniko.jrcichra.dev/kaniko | jq -r '.name')
+export JOBNAME=$(curl --fail -X POST -s -H 'Content-Type: application/json' -H "CF-Access-Client-Id: 46ea1be6ad585778416864f114d5cff1.access" -H "CF-Access-Client-Secret: ${ORACLE_K8S_ACCESS_TOKEN}" -d "${JSON}" https://kaniko.jrcichra.dev/kaniko | jq -r '.name')
 
 CONTINUE=true
 while "$CONTINUE";do
