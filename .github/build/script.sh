1974ed79906ededd777a85e999f5f12fdfb99bc4
Justin Cichra <1342149+jrcichra@users.noreply.github.com>
2022-02-19 02:27:43 -0500

    try with a file

--- .github/build/script.sh
+++ .github/build/script.sh
@@ -2,10 +2,11 @@
 set -x
 export TIRA_SHA=${TIRA_SHA::8}
 JSON=$(cat .github/build/request.json | envsubst)
-JOBNAME=$(curl --fail -X POST -s -H 'Content-Type: application/json' -H "CF-Access-Client-Id: 46ea1be6ad585778416864f114d5cff1.access" -H "CF-Access-Client-Secret: ${ORACLE_K8S_ACCESS_TOKEN}" -d "${JSON}" https://kaniko.jrcichra.dev/kaniko)
+JOBNAME=$(curl --fail -X POST -s -H 'Content-Type: application/json' -H "CF-Access-Client-Id: 46ea1be6ad585778416864f114d5cff1.access" -H "CF-Access-Client-Secret: ${ORACLE_K8S_ACCESS_TOKEN}" -d "${JSON}" https://kaniko.jrcichra.dev/kaniko | jq -r '.name')
 
 CONTINUE=true
 while "$CONTINUE";do
-curl -s -H 'Content-Type: application/json' -H "CF-Access-Client-Id: 46ea1be6ad585778416864f114d5cff1.access" -H "CF-Access-Client-Secret: ${ORACLE_K8S_ACCESS_TOKEN}" https://kaniko.jrcichra.dev/kaniko  -d "{\"name\": \"${JOBNAME}\"}" | jq -r '.message' | grep -q 'completed successfully' && CONTINUE=false
 sleep 10
+JSON=$(cat .github/build/progress.json | envsubst)
+curl -s -H 'Content-Type: application/json' -H "CF-Access-Client-Id: 46ea1be6ad585778416864f114d5cff1.access" -H "CF-Access-Client-Secret: ${ORACLE_K8S_ACCESS_TOKEN}" https://kaniko.jrcichra.dev/kaniko  -d "${JSON}" | jq -r '.message' | grep -q 'completed successfully' && CONTINUE=false
 done
