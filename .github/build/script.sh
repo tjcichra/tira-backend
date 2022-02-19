a9b7a74a333ec25cc18596da788ea0fcfd3a8e68
Justin Cichra <1342149+jrcichra@users.noreply.github.com>
2022-02-19 13:24:59 -0500

    try combining manifests

--- .github/build/script.sh
+++ .github/build/script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -x
-export TIRA_SHA=${TIRA_SHA::8}
+export TIRA_SHA="sha-${TIRA_SHA::8}"
 export EPOCH=$(date +%s)
 JSON=$(cat .github/build/request.json | envsubst)
 export JOBNAME=$(curl --fail -X POST -s -H 'Content-Type: application/json' -H "CF-Access-Client-Id: 46ea1be6ad585778416864f114d5cff1.access" -H "CF-Access-Client-Secret: ${ORACLE_K8S_ACCESS_TOKEN}" -d "${JSON}" https://kaniko.jrcichra.dev/kaniko | jq -r '.name')
