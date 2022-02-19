a6c6ed4774c7746d0968dd6f1b03432b4184d446
Justin Cichra <1342149+jrcichra@users.noreply.github.com>
2022-02-19 15:54:49 -0500

    simple manifest create

--- .github/build/manifest.sh
+++ .github/build/manifest.sh
@@ -3,22 +3,12 @@ set -x
 # do it for the sha
 export TIRA_SHA="sha-${TIRA_SHA::8}"
 docker manifest create ghcr.io/tjcichra/tira_backend:${TIRA_SHA} --amend \
-ghcr.io/tjcichra/tira_backend:${TIRA_SHA} \
-ghcr.io/tjcichra/tira_backend_arm64:${TIRA_SHA}
-docker manifest annotate ghcr.io/tjcichra/tira_backend:${TIRA_SHA} \
-ghcr.io/tjcichra/tira_backend_amd64:${TIRA_SHA} --arch amd64
-docker manifest annotate ghcr.io/tjcichra/tira_backend:${TIRA_SHA} \
-ghcr.io/tjcichra/tira_backend_arm64:${TIRA_SHA} --arch arm64
-docker manifest inspect ghcr.io/tjcichra/tira_backend:${TIRA_SHA}
+    ghcr.io/tjcichra/tira_backend_amd64:${TIRA_SHA} \
+    ghcr.io/tjcichra/tira_backend_arm64:${TIRA_SHA}
 docker manifest push ghcr.io/tjcichra/tira_backend:${TIRA_SHA}
 # now for latest
 export TIRA_SHA="latest"
 docker manifest create ghcr.io/tjcichra/tira_backend:${TIRA_SHA} --amend \
-ghcr.io/tjcichra/tira_backend:${TIRA_SHA} \
-ghcr.io/tjcichra/tira_backend_arm64:${TIRA_SHA}
-docker manifest annotate ghcr.io/tjcichra/tira_backend:${TIRA_SHA} \
-ghcr.io/tjcichra/tira_backend_amd64:${TIRA_SHA} --arch amd64
-docker manifest annotate ghcr.io/tjcichra/tira_backend:${TIRA_SHA} \
-ghcr.io/tjcichra/tira_backend_arm64:${TIRA_SHA} --arch arm64
-docker manifest inspect ghcr.io/tjcichra/tira_backend:${TIRA_SHA}
+    ghcr.io/tjcichra/tira_backend_amd64:${TIRA_SHA} \
+    ghcr.io/tjcichra/tira_backend_arm64:${TIRA_SHA}
 docker manifest push ghcr.io/tjcichra/tira_backend:${TIRA_SHA}
