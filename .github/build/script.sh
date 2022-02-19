1452133ea0045ebd4f4764d13073d2e2b8d8791f
Justin Cichra <1342149+jrcichra@users.noreply.github.com>
2022-02-19 02:08:23 -0500

    wait for it to finish

--- .github/build/script.sh
+++ .github/build/script.sh
@@ -2,4 +2,9 @@
 set -x
 export TIRA_SHA=${TIRA_SHA::8}
 JSON=$(cat .github/build/request.json | envsubst)
-curl --fail -X POST -s -H 'Content-Type: application/json' -H "CF-Access-Client-Id: 46ea1be6ad585778416864f114d5cff1.access" -H "CF-Access-Client-Secret: ${ORACLE_K8S_ACCESS_TOKEN}" -d "${JSON}" https://kaniko.jrcichra.dev/kaniko
+JOBNAME=$(curl --fail -X POST -s -H 'Content-Type: application/json' -H "CF-Access-Client-Id: 46ea1be6ad585778416864f114d5cff1.access" -H "CF-Access-Client-Secret: ${ORACLE_K8S_ACCESS_TOKEN}" -d "${JSON}" https://kaniko.jrcichra.dev/kaniko)
+
+CONTINUE=true
+while "$CONTINUE";do
+curl -s -H 'Content-Type: application/json' -H "CF-Access-Client-Id: 46ea1be6ad585778416864f114d5cff1.access" -H "CF-Access-Client-Secret: ${ORACLE_K8S_ACCESS_TOKEN}" https://kaniko.jrcichra.dev/kaniko | jq -r '.message' | grep -q 'completed successfully' && CONTINUE=false
+done
