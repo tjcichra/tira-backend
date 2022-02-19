e60a4552abb798d2df2602a0847a9481c90dd2a4
Justin Cichra <1342149+jrcichra@users.noreply.github.com>
2022-02-19 01:44:07 -0500

    try a script

---
+++ .github/build/script.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -x
+export TIRA_SHA=${TIRA_SHA::8}
+JSON=$(cat .github/build/request.json | envsubst)
+curl -X POST -s -H 'Content-Type: application/json' -H "CF-Access-Client-Id: 46ea1be6ad585778416864f114d5cff1.access" -H "CF-Access-Client-Secret: ${ORACLE_K8S_ACCESS_TOKEN}" -d "${JSON}" https://kaniko.jrcichra.dev/kaniko
