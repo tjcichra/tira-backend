0fe2f9d0f9f0f61a37b0301a239ba3c1bece9e57
Justin Cichra <1342149+jrcichra@users.noreply.github.com>
2022-02-19 21:30:49 -0500

    add annotations

--- .github/build/manifest.sh
+++ .github/build/manifest.sh
@@ -5,6 +5,14 @@ export TIRA_SHA="sha-${TIRA_SHA::8}"
 docker manifest create ghcr.io/tjcichra/tira_backend:${TIRA_SHA} \
     --amend ghcr.io/tjcichra/tira_backend_amd64:${TIRA_SHA} \
     --amend ghcr.io/tjcichra/tira_backend_arm64:${TIRA_SHA}
+docker manifest annotate ghcr.io/tjcichra/tira_backend:${TIRA_SHA} \
+    ghcr.io/tjcichra/tira_backend_amd64:${TIRA_SHA} \
+    --os linux \
+    --arch amd64
+docker manifest annotate ghcr.io/tjcichra/tira_backend:${TIRA_SHA} \
+    ghcr.io/tjcichra/tira_backend_arm64:${TIRA_SHA} \
+    --os linux \
+    --arch arm64
 docker manifest push ghcr.io/tjcichra/tira_backend:${TIRA_SHA}
 
 # set the current sha to the latest if we're on the default branch
@@ -12,5 +20,13 @@ if [ "$DEFAULT_BRANCH" == "main" ] || [ "$DEFAULT_BRANCH" == "master" ]; then
     docker manifest create ghcr.io/tjcichra/tira_backend:latest \
         --amend ghcr.io/tjcichra/tira_backend_amd64:${TIRA_SHA} \
         --amend ghcr.io/tjcichra/tira_backend_arm64:${TIRA_SHA}
+    docker manifest annotate ghcr.io/tjcichra/tira_backend:${TIRA_SHA} \
+        ghcr.io/tjcichra/tira_backend_amd64:${TIRA_SHA} \
+        --os linux \
+        --arch amd64
+    docker manifest annotate ghcr.io/tjcichra/tira_backend:${TIRA_SHA} \
+        ghcr.io/tjcichra/tira_backend_arm64:${TIRA_SHA} \
+        --os linux \
+        --arch arm64
     docker manifest push ghcr.io/tjcichra/tira_backend:latest
 fi
