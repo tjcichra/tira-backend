19f652149a23a86c1b9205ccdfa157b3c61a1c8b
Justin Cichra <1342149+jrcichra@users.noreply.github.com>
2022-02-19 20:45:22 -0500

    try combining manifests (#7)
    * try combining manifests

---
+++ .github/build/manifest.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -x
+# do it for the sha
+export TIRA_SHA="sha-${TIRA_SHA::8}"
+docker manifest create ghcr.io/tjcichra/tira_backend:${TIRA_SHA} \
+    --amend ghcr.io/tjcichra/tira_backend_amd64:${TIRA_SHA} \
+    --amend ghcr.io/tjcichra/tira_backend_arm64:${TIRA_SHA}
+docker manifest push ghcr.io/tjcichra/tira_backend:${TIRA_SHA}
+
+# set the current sha to the latest if we're on the default branch
+if [ "$DEFAULT_BRANCH" == "main" ] || [ "$DEFAULT_BRANCH" == "master" ]; then
+    docker manifest create ghcr.io/tjcichra/tira_backend:latest \
+        --amend ghcr.io/tjcichra/tira_backend_amd64:${TIRA_SHA} \
+        --amend ghcr.io/tjcichra/tira_backend_arm64:${TIRA_SHA}
+    docker manifest push ghcr.io/tjcichra/tira_backend:latest
+fi
