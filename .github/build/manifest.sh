ad0643ef930aa2155b1775d8aeea177bfa96e56a
Justin Cichra <1342149+jrcichra@users.noreply.github.com>
2022-02-20 00:25:45 -0500

    try all with one image

--- .github/build/manifest.sh
+++ .github/build/manifest.sh
@@ -4,35 +4,21 @@ set -x
 # env vars
 export TIRA_SHA="sha-${TIRA_SHA::8}"
 # pull the containers
-docker pull ghcr.io/tjcichra/tira_backend_amd64:${TIRA_SHA}
-docker pull ghcr.io/tjcichra/tira_backend_arm64:${TIRA_SHA}
+docker pull ghcr.io/tjcichra/tira_backend:${TIRA_SHA}-amd64
+docker pull ghcr.io/tjcichra/tira_backend:${TIRA_SHA}-arm64
+
+#https://www.docker.com/blog/multi-arch-build-and-images-the-simple-way/
 
 # update the current sha on the base image
 docker manifest create ghcr.io/tjcichra/tira_backend:${TIRA_SHA} \
-    --amend ghcr.io/tjcichra/tira_backend_amd64:${TIRA_SHA} \
-    --amend ghcr.io/tjcichra/tira_backend_arm64:${TIRA_SHA}
-docker manifest annotate ghcr.io/tjcichra/tira_backend:${TIRA_SHA} \
-    ghcr.io/tjcichra/tira_backend_amd64:${TIRA_SHA} \
-    --os linux \
-    --arch amd64
-docker manifest annotate ghcr.io/tjcichra/tira_backend:${TIRA_SHA} \
-    ghcr.io/tjcichra/tira_backend_arm64:${TIRA_SHA} \
-    --os linux \
-    --arch arm64
+    --amend ghcr.io/tjcichra/tira_backend:${TIRA_SHA}-amd64 \
+    --amend ghcr.io/tjcichra/tira_backend:${TIRA_SHA}-arm64
 docker manifest push ghcr.io/tjcichra/tira_backend:${TIRA_SHA}
 
 # if main branch, update the latest tag on the base image
 if [ "$BRANCH_NAME" == "main" ] || [ "$BRANCH_NAME" == "master" ]; then
     docker manifest create ghcr.io/tjcichra/tira_backend:latest \
-        --amend ghcr.io/tjcichra/tira_backend_amd64:${TIRA_SHA} \
-        --amend ghcr.io/tjcichra/tira_backend_arm64:${TIRA_SHA}
-    docker manifest annotate ghcr.io/tjcichra/tira_backend:latest \
-        ghcr.io/tjcichra/tira_backend_amd64:${TIRA_SHA} \
-        --os linux \
-        --arch amd64
-    docker manifest annotate ghcr.io/tjcichra/tira_backend:latest \
-        ghcr.io/tjcichra/tira_backend_arm64:${TIRA_SHA} \
-        --os linux \
-        --arch arm64
+        --amend ghcr.io/tjcichra/tira_backend:${TIRA_SHA}-amd64 \
+        --amend ghcr.io/tjcichra/tira_backend:${TIRA_SHA}-arm64
     docker manifest push ghcr.io/tjcichra/tira_backend:latest
 fi
