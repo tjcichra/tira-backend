abbb9e49daca9b5639fc4a4d2a232499259f4acf
tjcichra <8120075+tjcichra@users.noreply.github.com>
2022-04-23 16:29:41 -0400

    added endpoint env var

--- src/service/images.rs
+++ src/service/images.rs
@@ -1,7 +1,8 @@
 use std::env;
 
 use aws_config::meta::region::RegionProviderChain;
-use aws_sdk_s3::{ByteStream, Client, Region};
+use aws_sdk_s3::{ByteStream, Client, Region, Endpoint};
+use rocket::http::hyper::Uri;
 
 /// Service function for uploading an image.
 pub async fn upload_image(file_name: &str, bytes: Vec<u8>) {
@@ -25,10 +26,12 @@ pub async fn upload_image(file_name: &str, bytes: Vec<u8>) {
 
 pub async fn load_image(file_name: &str) -> Vec<u8> {
     let bucket_name = env::var("IMAGE_BUCKET_NAME").unwrap();
+    let bucket_endpoint = env::var("IMAGE_ENDPOINT_URI").unwrap();
 
     let region_provider = RegionProviderChain::first_try(Region::new("us-east-1"));
     let shared_config = aws_config::from_env().region(region_provider).load().await;
-    let client = Client::new(&shared_config);
+    let shared_config = aws_sdk_s3::config::Builder::from(&shared_config).endpoint_resolver(Endpoint::immutable(bucket_endpoint.parse().unwrap())).build();
+    let client = Client::from_conf(shared_config);
 
     let resp = client
         .get_object()
