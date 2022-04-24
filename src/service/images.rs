4526451460da926f725178a886bc793e623ae52b
tjcichra <8120075+tjcichra@users.noreply.github.com>
2022-04-24 16:17:52 -0400

    added minio support

--- src/service/images.rs
+++ src/service/images.rs
@@ -1,16 +1,15 @@
 use std::env;
 
-use aws_config::meta::region::RegionProviderChain;
-use aws_sdk_s3::{ByteStream, Client, Region, Endpoint};
-use rocket::http::hyper::Uri;
+use aws_sdk_s3::{ByteStream, Client, Endpoint};
 
 /// Service function for uploading an image.
 pub async fn upload_image(file_name: &str, bytes: Vec<u8>) {
     let bucket_name = env::var("IMAGE_BUCKET_NAME").unwrap();
+    let bucket_endpoint = env::var("IMAGE_ENDPOINT_URI").unwrap();
 
-    let region_provider = RegionProviderChain::first_try(Region::new("us-east-1"));
-    let shared_config = aws_config::from_env().region(region_provider).load().await;
-    let client = Client::new(&shared_config);
+    let shared_config = aws_config::load_from_env().await;
+    let shared_config = aws_sdk_s3::config::Builder::from(&shared_config).endpoint_resolver(Endpoint::immutable(bucket_endpoint.parse().unwrap())).build();
+    let client = Client::from_conf(shared_config);
 
     let body = ByteStream::from(bytes);
 
@@ -28,8 +27,7 @@ pub async fn load_image(file_name: &str) -> Vec<u8> {
     let bucket_name = env::var("IMAGE_BUCKET_NAME").unwrap();
     let bucket_endpoint = env::var("IMAGE_ENDPOINT_URI").unwrap();
 
-    let region_provider = RegionProviderChain::first_try(Region::new("us-east-1"));
-    let shared_config = aws_config::from_env().region(region_provider).load().await;
+    let shared_config = aws_config::load_from_env().await;
     let shared_config = aws_sdk_s3::config::Builder::from(&shared_config).endpoint_resolver(Endpoint::immutable(bucket_endpoint.parse().unwrap())).build();
     let client = Client::from_conf(shared_config);
 
